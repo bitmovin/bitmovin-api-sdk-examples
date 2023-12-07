8ef4be06c8ce2f0a84da1f28a69e209c953db5d8
KnoblauchPilze <maxime.rigaud@bitmovin.com>
2023-12-07 15:23:32 +0100

    fix: mkdir is md

--- go/run-example.bat
+++ go/run-example.bat
@@ -3,7 +3,7 @@ shift
 
 go mod init github.com/bitmovin/bitmovin-api-sdk-examples
 go get github.com/bitmovin/bitmovin-api-sdk-go
-mkdir -p build
+md build
 go build -o build cmd/%file_name%.go
 
 ./build/$%file_name%.go %cd%/example.properties %*
