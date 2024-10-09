6d638a990282b7f944e430d4f1833026ce2a4aef
Robert <robert.jacobi@bitmovin.com>
2024-10-09 14:39:34 +0200

    Add Java example

---
+++ java/src/main/java/EncodingTemplates.java
@@ -0,0 +1,153 @@
+import com.bitmovin.api.sdk.BitmovinApi;
+import com.bitmovin.api.sdk.common.BitmovinException;
+import com.bitmovin.api.sdk.model.EncodingTemplateStartResponse;
+import com.bitmovin.api.sdk.model.MessageType;
+import com.bitmovin.api.sdk.model.Output;
+import com.bitmovin.api.sdk.model.S3Output;
+import com.bitmovin.api.sdk.model.Status;
+import com.bitmovin.api.sdk.model.Task;
+import common.ConfigProvider;
+import feign.Logger.Level;
+import feign.slf4j.Slf4jLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * This example shows how to do a Per-Title encoding with default manifests. For more information
+ * see: https://bitmovin.com/per-title-encoding/
+ *
+ * <p>The following configuration parameters are expected:
+ *
+ * <ul>
+ *   <li>BITMOVIN_API_KEY - Your API key for the Bitmovin API
+ *   <li>BITMOVIN_TENANT_ORG_ID - (optional) The ID of the Organisation in which you want to perform
+ *       the encoding.
+ *   <li>HTTP_INPUT_FILE_PATH - The path to your input file on the provided HTTP server Example:
+ *       videos/1080p_Sintel.mp4
+ *   <li>S3_OUTPUT_BUCKET_NAME - The name of your S3 output bucket. Example: my-bucket-name
+ *   <li>S3_OUTPUT_ACCESS_KEY - The access key of your S3 output bucket
+ *   <li>S3_OUTPUT_SECRET_KEY - The secret key of your S3 output bucket
+ *   <li>S3_OUTPUT_BASE_PATH - The base path on your S3 output bucket where content will be written.
+ *       Example: /outputs
+ * </ul>
+ *
+ * <p>Configuration parameters will be retrieved from these sources in the listed order:
+ *
+ * <ol>
+ *   <li>command line arguments (eg BITMOVIN_API_KEY=xyz)
+ *   <li>properties file located in the root folder of the JAVA examples at ./examples.properties
+ *       (see examples.properties.template as reference)
+ *   <li>environment variables
+ *   <li>properties file located in the home folder at ~/.bitmovin/examples.properties (see
+ *       examples.properties.template as reference)
+ * </ol>
+ */
+public class EncodingTemplates {
+
+  private static final Logger logger = LoggerFactory.getLogger(EncodingTemplates.class);
+
+  private static BitmovinApi bitmovinApi;
+
+  public static void main(String[] args) throws Exception {
+    ConfigProvider configProvider = new ConfigProvider(args);
+    bitmovinApi =
+        BitmovinApi.builder()
+            .withApiKey(configProvider.getBitmovinApiKey())
+            // uncomment the following line if you are working with a multi-tenant account
+            // .withTenantOrgId(configProvider.getBitmovinTenantOrgId())
+            .withLogger(
+                new Slf4jLogger(), Level.BASIC) // set the logger and log level for the API client
+            .build();
+
+    Output output =
+        createS3Output(
+            configProvider.getS3OutputBucketName(),
+            configProvider.getS3OutputAccessKey(),
+            configProvider.getS3OutputSecretKey());
+
+    String template = new String(Files.readAllBytes(Paths.get("./encodingTemplate.yml")));
+    String interpolatedTemplate = template
+            .replaceAll("\\{\\{input.path}}", configProvider.getHttpInputFilePath())
+            .replaceAll("\\{\\{output.id}}", output.getId())
+            .replaceAll("\\{\\{output.path}}", configProvider.getS3OutputBasePath());
+
+    executeEncoding(interpolatedTemplate);
+  }
+
+  /**
+   * Creates a resource representing an AWS S3 cloud storage bucket to which generated content will
+   * be transferred. For alternative output methods see <a
+   * href="https://bitmovin.com/docs/encoding/articles/supported-input-output-storages">list of
+   * supported input and output storages</a>
+   *
+   * <p>The provided credentials need to allow <i>read</i>, <i>write</i> and <i>list</i> operations.
+   * <i>delete</i> should also be granted to allow overwriting of existings files. See <a
+   * href="https://bitmovin.com/docs/encoding/faqs/how-do-i-create-a-aws-s3-bucket-which-can-be-used-as-output-location">creating
+   * an S3 bucket and setting permissions</a> for further information
+   *
+   * <p>For reasons of simplicity, a new output resource is created on each execution of this
+   * example. In production use, this method should be replaced by a <a
+   * href="https://bitmovin.com/docs/encoding/api-reference/sections/outputs#/Encoding/GetEncodingOutputsS3">get
+   * call</a> retrieving an existing resource.
+   *
+   * <p>API endpoint:
+   * https://bitmovin.com/docs/encoding/api-reference/sections/outputs#/Encoding/PostEncodingOutputsS3
+   *
+   * @param bucketName The name of the S3 bucket
+   * @param accessKey The access key of your S3 account
+   * @param secretKey The secret key of your S3 account
+   */
+  private static S3Output createS3Output(String bucketName, String accessKey, String secretKey)
+      throws BitmovinException {
+
+    S3Output s3Output = new S3Output();
+    s3Output.setBucketName(bucketName);
+    s3Output.setAccessKey(accessKey);
+    s3Output.setSecretKey(secretKey);
+
+    return bitmovinApi.encoding.outputs.s3.create(s3Output);
+  }
+
+  /**
+   * Starts the actual encoding process and periodically polls its status until it reaches a final
+   * state
+   *
+   * <p>API endpoints:
+   * https://bitmovin.com/docs/encoding/api-reference/all#/Encoding/PostEncodingEncodingsStartByEncodingId
+   * https://developer.bitmovin.com/encoding/reference/getencodingencodingsstatusbyencodingid
+   *
+   * <p>Please note that you can also use our webhooks API instead of polling the status. For more
+   * information consult the API spec:
+   * https://developer.bitmovin.com/encoding/reference/getnotificationswebhooksencodingencodingsfinished
+   *
+   * @param template The Encoding Template to be used for starting an encoding
+   */
+  private static void executeEncoding(String template)
+      throws InterruptedException, BitmovinException {
+    EncodingTemplateStartResponse response = bitmovinApi.encoding.templates.start(template);
+
+    Task task;
+    do {
+      Thread.sleep(5000);
+      task = bitmovinApi.encoding.encodings.status(response.getEncodingId());
+      logger.info("Encoding status is {} (progress: {} %)", task.getStatus(), task.getProgress());
+    } while (task.getStatus() != Status.FINISHED
+        && task.getStatus() != Status.ERROR
+        && task.getStatus() != Status.CANCELED);
+
+    if (task.getStatus() == Status.ERROR) {
+      logTaskErrors(task);
+      throw new RuntimeException("Encoding failed");
+    }
+    logger.info("Encoding finished successfully");
+  }
+
+  private static void logTaskErrors(Task task) {
+    task.getMessages().stream()
+        .filter(msg -> msg.getType() == MessageType.ERROR)
+        .forEach(msg -> logger.error(msg.getText()));
+  }
+}
